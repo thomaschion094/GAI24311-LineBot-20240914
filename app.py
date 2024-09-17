e727b23453db8bb089d98c627626fd82ed863c9f
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-17 15:45:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def linebot():
