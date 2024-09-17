a9e99e3a13ef5ae833f57bd5dcbbe227b66c0a63
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-17 15:26:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-def linebot(request):
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
-        access_token = os.environ.get('LINE_BOT_ACCESS_TOKEN')
-        secret = os.environ.get('LINE_BOT_SECRET')
-        body = request.get_data(as_text=True)
-        json_data = json.loads(body)
-        line_bot_api = LineBotApi(access_token)
-        handler = WebhookHandler(secret)
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        msg = json_data['events'][0]['message']['text']
-        tk = json_data['events'][0]['replyToken']
-        line_bot_api.reply_message(tk,TextSendMessage(msg))
-        print(msg, tk)
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = '你的 access token'
+        secret = '你的 Channel secret'
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        line_bot_api.reply_message(tk,TextSendMessage(msg))  # 回傳訊息
+        print(msg, tk)                                       # 印出內容
     except:
-        print(request.args)
-    return 'OK'
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'      
