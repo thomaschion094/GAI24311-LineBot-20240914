f0c4aa474ae9c65107d13781e2a0ef81f18888b5
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-25 15:00:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, QuickReply, QuickReplyButton, MessageAction
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from pymongo import MongoClient
+from Query import query_region
 
 app = Flask(__name__)
 
@@ -17,8 +21,8 @@ secret = os.environ.get('LINE_BOT_SECRET')
 line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
 handler = WebhookHandler(secret)                     # 確認 secret 是否正確
 
-@app.route("/", methods=['POST'])
-def linebot():
+@app.route("/callback", methods=['POST'])
+def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
@@ -45,79 +49,118 @@ collection = db['taipei']
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg=str(event.message.text)
-    if msg == '選單一':
-        # reply='這是選單一'
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage(reply))
+    #if msg == '士林':
+    if '士林' in msg:
+         #reply='這是選單一'
+         #line_bot_api.reply_message(event.reply_token, TextSendMessage(reply))
         #遦線到mondb並查詢士林區的資料
-        try:
-            #client = MongoClient(MONGODB_URI)
-            #db = client['CareDB']
-            #collection = db['taipei']
-            print('連線成功')
-            # 查詢資料
-            query = {
-                "$or": [
-                    {"行政區": "士林區"},
-                    {"區域別": "士林區"}
-                ]
-            }
+         try:
+             #client = MongoClient(MONGODB_URI)
+             #db = client['CareDB']
+             #collection = db['taipei']
+             print('連線成功')
+             # 查詢資料
+             query = {
+                 "$or": [
+                     {"行政區": "士林區"},
+                     {"區域別": "士林區"}
+                 ]
+             }
 
-            result = collection.find(query)
-            response = ""
-            for item in result:
-                name = item.get('據點名稱') or item.get('機構名稱')
-                address = item.get('據點地址') or item.get('地址')
-                phone = item.get('電話')
-                response += f"據點名稱: {name}\n地址: {address}\n電話: {phone}\n\n"
+             result = collection.find(query)
+             response = ""
+             for item in result:
+                 name = item.get('據點名稱') or item.get('機構名稱')
+                 address = item.get('據點地址') or item.get('地址')
+                 phone = item.get('電話')
+                 response += f"據點名稱: {name}\n地址: {address}\n電話: {phone}\n\n"
 
-            if not response:
-                response = "查無資料"
+             if not response:
+                 response = "查無資料"
 
             
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
-        except Exception as e:
-            print(f'連線失敗: {e}')
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'連線失敗: {e}'))
-        finally:
-            client.close()
-    elif msg == '選單二':
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+         except Exception as e:
+             print(f'連線失敗: {e}')
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'連線失敗: {e}'))
+        #  finally:
+        #      client.close()
+    #elif msg == '選單二':
+    elif '大同' in msg:
         try:
-            message = TextSendMessage(
-            text='請選擇最喜歡的季節',
-            quick_reply=QuickReply(
-                items=[
-                    QuickReplyButton(
-                        action=MessageAction(label="春", text="春")
-                    ),
-                    QuickReplyButton(
-                        action=MessageAction(label="夏", text="夏")
-                    ),
-                    QuickReplyButton(
-                        action=MessageAction(label="秋", text="秋")
-                    ),
-                    QuickReplyButton(
-                        action=MessageAction(label="冬", text="冬")
-                    ),
-                ]
-            )
-        )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-    elif msg =='選單三':
-            reply = TextSendMessage(
-                text='$你選擇了選單3',
-                emojis=[
-                    {
-                        'index': 0,
-                        'productId': '5ac1bfd5040ab15980c9b435',
-                        'emojiId': '003'
-                    },
-                ]
-            )
-            line_bot_api.reply_message(event.reply_token, reply)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+             #client = MongoClient(MONGODB_URI)
+             #db = client['CareDB']
+             #collection = db['taipei']
+             print('連線成功')
+             # 查詢資料
+             query = {
+                 "$or": [
+                     {"行政區": "大同區"},
+                     {"區域別": "大同區"}
+                 ]
+             }
+
+             result = collection.find(query)
+             response = ""
+             for item in result:
+                 name = item.get('據點名稱') or item.get('機構名稱')
+                 address = item.get('據點地址') or item.get('地址')
+                 phone = item.get('電話')
+                 response += f"據點名稱: {name}\n地址: {address}\n電話: {phone}\n\n"
+
+             if not response:
+                 response = "查無資料"
+
+            
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+        except Exception as e:
+             print(f'連線失敗: {e}')
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'連線失敗: {e}'))
+    #elif msg =='選單三':
+    elif '北投' in msg:
+        try:
+             #client = MongoClient(MONGODB_URI)
+             #db = client['CareDB']
+             #collection = db['taipei']
+             print('連線成功')
+             # 查詢資料
+             query = {
+                 "$or": [
+                     {"行政區": "北投區"},
+                     {"區域別": "北投區"}
+                 ]
+             }
+
+             result = collection.find(query)
+             response = ""
+             for item in result:
+                 name = item.get('據點名稱') or item.get('機構名稱')
+                 address = item.get('據點地址') or item.get('地址')
+                 phone = item.get('電話')
+                 response += f"據點名稱: {name}\n地址: {address}\n電話: {phone}\n\n"
+
+             if not response:
+                 response = "查無資料"
+
+            
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+        except Exception as e:
+             print(f'連線失敗: {e}')
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'連線失敗: {e}'))
+    
+    #         reply = TextSendMessage(
+    #             text='$你選擇了選單3',
+    #             emojis=[
+    #                 {
+    #                     'index': 0,
+    #                     'productId': '5ac1bfd5040ab15980c9b435',
+    #                     'emojiId': '003'
+    #                 },
+    #             ]
+    #         )
+    #         line_bot_api.reply_message(event.reply_token, reply)
+    # else:
+    #     line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
 
 if __name__ == "__main__":
     app.run()
